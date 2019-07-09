b663ba2a38044e936da99ae570247748601cc28a
Emily Costa <ecost020@fiu.edu>
2019-07-08 20:49:19 -0400

    Added Stuff

---
+++ ML_cry_classification.py
@@ -0,0 +1,9 @@
+"""
+Created on: 7.8.19
+Author: Emily Costa, ...
+The heart of ML algorithm, should compute on already balanced data
+My current idea
+""""
+
+import numpy as np
+import 
---
+++ processing.py
@@ -0,0 +1,6 @@
+""""
+Created on: 7.8.19
+Author: Emily Costa, ...
+This program processes data prior to running through ML algorithm.
+My current idea is to do some numeric approach to decide which data is worth keeping
+"""
