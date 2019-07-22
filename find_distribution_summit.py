82109b20ac518554edba956cb54429f39e4aae76
Emily Costa <36118116+emilyjcosta5@users.noreply.github.com>
2019-07-22 17:22:21 -0400

    Added description

--- find_distribution_summit.py
+++ find_distribution_summit.py
@@ -1,3 +1,9 @@
+'''
+Script for finding distribution of space groups in a directory with h5 files then saves to JSON file
+Created on: July 22, 2019
+Author: Emily Costa
+'''
+
 from processing import iterate_through_data, save_space_grp_distribution
 
 h5_path = '/gpfs/alpine/world-shared/stf011/junqi/smc/train/'
