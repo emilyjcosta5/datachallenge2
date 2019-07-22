b187af71c699e58bf17a48a575afe33da7928d50
Emily Costa <36118116+emilyjcosta5@users.noreply.github.com>
2019-07-22 10:25:04 -0400

    Update find_distribution_summit.py

--- find_distribution_summit.py
+++ find_distribution_summit.py
@@ -1,5 +1,5 @@
 from processing import iterate_through_data, save_space_grp_distribution
 
 h5_path = '/gpfs/alpine/world-shared/stf011/junqi/smc/'
-dict_dist = iterate_through_data(h5_path, save_fig=True)
-save_space_grp_distribution(dict_dist)
+dict_dist = iterate_through_data(h5_path, save_fig=True, fig_name='dist_on_summit')
+save_space_grp_distribution(dict_dist, file_name='dist_on_summit')
