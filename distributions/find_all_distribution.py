989815b08baa3f816e40553f69cf382acef310e8
Emily Costa <ecost020@fiu.edu>
2019-07-27 14:10:55 -0400

    Started finding overall distribution

---
+++ distributions/find_all_distribution.py
@@ -0,0 +1,11 @@
+'''
+For finding and visualizing distributions from HDF5 files containing Crystallographic imaging data.
+Created on: 7.27.2019
+Author: Emily Costa
+'''
+
+def convert_JSON_to_dict(j):
+    with open(j, 'r') as f:
+        data
+
+def make_JSON_from_Dicts(
