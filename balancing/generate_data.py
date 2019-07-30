966c37f4edf0554f1367d7aebc0bed87a88f1aac
Emily Costa <ecost020@fiu.edu>
2019-07-29 22:27:00 -0400

    Created for functions and results from balancing dataset

---
+++ balancing/generate_data.py
@@ -0,0 +1,9 @@
+"""
+Functions to create synthetic data to balance crystallographic dataset
+Created on: July 20, 2019
+Author: Emily Costa
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
