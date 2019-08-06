13b0010593fdcbe337c11131ddd19a68a8365e22
Emily Costa <36118116+emilyjcosta5@users.noreply.github.com>
2019-08-06 09:46:47 -0400

    Create check_redist.py

---
+++ check_redist.py
@@ -0,0 +1,6 @@
+import h5py
+from distributions.functions.find_total_in_set import *
+from processing.processing import *
+
+js = ['processing/massagedDev.json', 'processing/massagedTest.json', 'processing/massagedTrain.json']
+keys = ['Dev','Test','Train']
