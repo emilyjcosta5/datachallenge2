8c80ef75bb617a56e64fab9fb2f8a8857fc4b861
Emily Costa <36118116+emilyjcosta5@users.noreply.github.com>
2019-08-06 12:14:39 -0400

    Update check_redist.py

--- check_redist.py
+++ check_redist.py
@@ -4,3 +4,7 @@ from processing.processing import *
 
 js = ['processing/massagedDev.json', 'processing/massagedTest.json', 'processing/massagedTrain.json']
 keys = ['Dev','Test','Train']
+
+dev_path = '/gpfs/alpine/world-shared/stf011/junqi/smc/dev'
+train_path = '/gpfs/alpine/world-shared/stf011/junqi/smc/train'
+test_path = '/gpfs/alpine/world-shared/stf011/junqi/smc/test'
