75af90083525e4fa60998b8dbc7873971c3528b0
Emily Costa <36118116+emilyjcosta5@users.noreply.github.com>
2019-08-09 11:22:20 -0400

    To run on overall

--- distributions/functions/expected_dist.py
+++ distributions/functions/expected_dist.py
@@ -38,3 +38,20 @@ def predict_dist(overall):
         
     with open('dev_redist.json', 'w') as de:
         json.dump(dev_redist, de)
+
+        
+if __name__ == '__main__':
+    json_path = 'overall_distribution.json'
+
+    with open(json_path, 'r') as f:
+        data = json.load(f)
+        nums = list(range(1,231))
+        data = np.array([data['Space Group {}'.format(num)] for num in nums])
+    
+    keys = np.arange(1, 231, dtype=int)
+    overall_dict = {}
+    for key,val in zip(keys,data):
+        overall_dict['Space Group {}'.format(key)] = val.item()
+    print('Dictionary created.')
+
+    predict_dist(overall_dict)
