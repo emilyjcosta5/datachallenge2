b86cd0c11b2969aa974be470820ddf915c479a7c
Emily Costa <36118116+emilyjcosta5@users.noreply.github.com>
2019-08-09 11:04:33 -0400

    Creates json files now

--- distributions/functions/expected_dist.py
+++ distributions/functions/expected_dist.py
@@ -3,5 +3,37 @@
 
 import numpy as np
 
-def predict_dist(overall, train, test, dev):
+def predict_dist(overall):
+    '''
+    parameters
+    ----------
+    overall: dictionary
+    overall amount of space groups in all datasets
     
+    returns
+    -------
+    '''
+    range = np.arange(1,231)
+    train_redist = {}
+    test_redist = {}
+    dev_redist = {}
+    for x in range:
+        sg = overall['Space Group {}'.format(x)]
+        if sg < 90:
+            train_redist['Space Group {}'.format(x)] = 30
+            test_redist['Space Group {}'.format(x)] = 30
+            dev_redist['Space Group {}'.format(x)] = 30
+        else:
+            y = int(sg/9)
+            train_redist['Space Group {}'.format(x)] = y*7
+            test_redist['Space Group {}'.format(x)] = y
+            dev_redist['Space Group {}'.format(x)] = y
+            
+    with open('train_redist.json', 'w') as tr:
+        json.dump(train_redist, tr)
+        
+    with open('test_redist.json', 'w') as te:
+        json.dump(test_redist, te)
+        
+    with open('dev_redist.json', 'w') as de:
+        json.dump(dev_redist, de)
